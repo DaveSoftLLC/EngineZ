0f59c0e6366246ca70f2988e8e11fc9a9efffdd6
BlazingAsher <BlazingAsher@users.noreply.github.com>
2018-04-28 17:04:06 -0400

    remove mysqlStuff function (depreciated)

--- mysqlTest.py
+++ mysqlTest.py
@@ -73,43 +73,4 @@ def mysqlInsert(user,score=None,data):
      db.commit()
      db.close()
      
-def mysqlStuff(insert,user="",score="",data=""):
-     if(insert):
-          
-          db = MySQLdb.connect(host=hosttxt,    # your host, usually localhost
-                     user=usertxt,         # your username
-                     passwd=passwdtxt,  # your password
-                     db=dbname)        # name of the data base
-
-          # you must create a Cursor object. It will let
-          #  you execute all the queries you need
-          cur = db.cursor()
-
-          # Use all the SQL you like
-          cur.execute("SELECT * from  leaderboard")
-
-          # print all the first cell of all the rows
-          for row in cur.fetchall():
-              print(row[0])
-
-          db.close()
-     else:
-          db = MySQLdb.connect(host=hosttxt,    # your host, usually localhost
-                     user=usertxt,         # your username
-                     passwd=passwdtxt,  # your password
-                     db=dbname)        # name of the data base
-
-          # you must create a Cursor object. It will let
-          #  you execute all the queries you need
-          cur = db.cursor()
-
-          # Use all the SQL you like
-          cur.execute("SELECT * FROM leaderboard")
-
-          # print all the first cell of all the rows
-          for row in cur.fetchall():
-              print(row[0])
-
-          db.close()
-mysqlStuff("hi");
 print(entryExists("lol"));

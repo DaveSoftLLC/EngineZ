03214c126a8813abdfed07d93017d6c772b4ad38
fairnightzz <zhehaizhang1@gmail.com>
2018-04-28 15:57:31 -0400

    almost no lag

--- gameClientZhehai.py
+++ gameClientZhehai.py
@@ -1,27 +1,27 @@
 import socket, threading
 from pygame import *
-TCP_IP = '192.227.178.111'
+TCP_IP = '10.88.214.97'
 TCP_PORT = 5005
 BUFFER_SIZE = 100
 running = True
 screen = display.set_mode((800,600))
-playerList = ["poop",(0,0)]
+playerList = ["Zhehai",(0,0)]
 otherPlayers = {}
 def getData():
     global BUFFER_SIZE
     global running
     global playerList
     global otherPlayers
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
     while running:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((TCP_IP, TCP_PORT))
         s.send(str(playerList).encode('utf-8'))
         data = eval(s.recv(BUFFER_SIZE).decode('utf-8'))
         try:
             otherPlayers = data
         except:
             pass
-        s.close()
+    s.close()
 threading.Thread(target=getData).start()
 while running:
     for e in event.get():

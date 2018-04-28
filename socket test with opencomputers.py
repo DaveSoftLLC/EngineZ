84dff16d173e3996c6d1d2c19a58b32a7b28e9f7
James Xu <36768789+jamesxu123@users.noreply.github.com>
2018-04-28 14:00:59 -0400

    Update socket test with opencomputers.py
    added threading functionality

--- socket test with opencomputers.py
+++ socket test with opencomputers.py
@@ -5,24 +5,28 @@ mixer.init()
 TCP_IP = '192.227.178.111'
 TCP_PORT = 5005
 BUFFER_SIZE = 100  # Normally 1024, but we want fast response
-def serverListen(TCP_IP, TCP_PORT):
-     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     s.bind((TCP_IP, TCP_PORT))
-     s.listen(1)
-     while 1:
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1):
+def listen():
+     while True:
           lis = ''
           conn, addr = s.accept()
-          print('Connection address:', addr)
-          data = conn.recv(BUFFER_SIZE)
-          if not data: continue
-          decoded = data.decode('utf-8')
-          
-          if decoded == 'quit':
-               conn.send('quitting'.encode('utf-8'))
-               print('quitting')
-               break
+          conn.settimeout(60)
+          threading.Thread(target = listenClient,args=(conn,addr)).start()
+def listenClient(conn,addr)
+     while True:
+          try:
+               data = conn.recv(BUFFER_SIZE)
+               if data:
+                    decoded = data.decode('utf-8')
+                    if decoded == 'quit':
+                         conn.send('quitting'.encode('utf-8'))
+                         print('quitting')
+                         break
+               else:
+                    print('No Data')
+          except:
+               print('Connection Broken')
      conn.close()
-def test(stuff):
-     print(stuff)
-     return 0
-listening = threading.Thread(target=serverListen, args=(TCP_IP,TCP_PORT,)).start()
+

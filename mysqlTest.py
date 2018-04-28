fc5febe5d3e521d838e93c52d21b2bb10ffb818b
BlazingAsher <BlazingAsher@users.noreply.github.com>
2018-04-28 17:03:03 -0400

    added functiopns for all mysql  commands

--- mysqlTest.py
+++ mysqlTest.py
@@ -26,6 +26,53 @@ def entryExists(user):
                db.close()
                return True
           
+def mysqlUpdate(user,score=None,data=""):
+     global hosttxt,usertxt,passwdtxt,dbname
+     db = MySQLdb.connect(host=hosttxt,    # your host, usually localhost
+                     user=usertxt,         # your username
+                     passwd=passwdtxt,  # your password
+                     db=dbname)        # name of the data base
+
+     # you must create a Cursor object. It will let
+     #  you execute all the queries you need
+     cur = db.cursor()
+
+     # Use all the SQL you like
+     if(score == None):
+          #update data
+          sqlcommand = "UPDATE leaderboard SET additionalData='" + data + "' WHERE user='"+ user "'"
+     else:
+          #update score
+          sqlcommand = "UPDATE leaderboard SET score='" + str(score) + "' WHERE user='"+ user "'"
+     
+     cur.execute(sqlcommand)
+
+     # print all the first cell of all the rows
+         
+     db.commit()
+     db.close()
+     
+def mysqlInsert(user,score=None,data):
+     global hosttxt,usertxt,passwdtxt,dbname
+     db = MySQLdb.connect(host=hosttxt,    # your host, usually localhost
+                     user=usertxt,         # your username
+                     passwd=passwdtxt,  # your password
+                     db=dbname)        # name of the data base
+
+     # you must create a Cursor object. It will let
+     #  you execute all the queries you need
+     cur = db.cursor()
+
+     # Use all the SQL you like
+     sqlcommand = "INSERT INTO leaderboard (playerName,score,additionalData) VALUES ('" user + "','" + str(score) + "','" + data"')"
+     
+     cur.execute(sqlcommand)
+
+     # print all the first cell of all the rows
+         
+     db.commit()
+     db.close()
+     
 def mysqlStuff(insert,user="",score="",data=""):
      if(insert):
           

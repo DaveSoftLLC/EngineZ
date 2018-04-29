282ff9452e3408156d7f9d4759237263e05a9662
James Xu <xujames007@gmail.com>
2018-04-29 10:10:55 -0400

    stuff

--- gameClientZhehai - Copy.py
+++ gameClientZhehai - Copy.py
@@ -25,7 +25,7 @@ textB="" #Text that will show for typing, saving
 typing=False
 agencyfont=font.SysFont("Agency FB",25)
 health = 100
-playerList = ["Zhehai",[1300,900],deg,state,health]
+playerList = ["James",[1300,900],deg,state,health]
 otherPlayers = {}
 background = image.load('Background/MapFinal.png')
 person = [image.load('Sprites/sprite1.png'),image.load('Sprites/sprite2.png'),image.load('Sprites/sprite3.png')]
@@ -40,7 +40,9 @@ def getData():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
     while running:
-        s.send(str(playerList+bullets).encode('utf-8'))
+        s.send((str(playerList)+'['+str(bullets)+']').encode('utf-8'))
+        data2 = s.recv(BUFFER_SIZE).decode('utf-8')
+        print(data2)
         data = eval(s.recv(BUFFER_SIZE).decode('utf-8'))
         try:
             otherPlayers = data
@@ -141,7 +143,7 @@ while running:
                 deg=degrees(atan2((screen.get_width()//2-nx),(screen.get_height()//2-ny)))
                 rotated = transform.rotate(person[otherPlayers[p][2]],otherPlayers[p][1])
                 screen.blit(rotated,(nx,ny))
-        bullets += p[4]
+        bullets += p[5]
 
     #Chat
     

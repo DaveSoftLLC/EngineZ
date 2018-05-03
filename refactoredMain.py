be5775de235f91c160a8ec63807ad705a6975397
JamesXu <xujames007@gmail.com>
2018-05-03 11:07:29 -0400

    added more content to Player class and removed some uesless files

--- refactoredMain.py
+++ refactoredMain.py
@@ -29,7 +29,7 @@ class GameMode:
             print("Error")
 
 class Player:
-    def __init__(self,name,pos,spritefiles,speed):
+    def __init__(self,game,name,pos,spritefiles,speed):
         self.sprites = spritefiles
         self.pos = pos
         self.rotation = 0
@@ -37,8 +37,10 @@ class Player:
         self.health = 100
         self.speed = speed
         self.bullets = []
+        self.game = game
+        self.rect = self.game.screen.blit(self.sprites[self.state],self.pos)
     def move(self,direction,map,collisionmap,speed=None):
-        if speed == None:
+        if speed is None:
             speed = self.speed
         if direction == 'UP':
             nx,ny = (self.pos[0],self.pos[1] - speed < map.get_height())
@@ -71,8 +73,10 @@ class Player:
     def fire(self):
         px,py = self.pos
         for a in range(1,6):
-            spread = deg+90-(3-a)*6
-            self.bullets.append([(px+5*cos(radians(spread)),py-5*sin(radians(spread))),spread])
+            angle = self.rotation+90-(3-a)*6
+            self.bullets.append([(px+5*cos(radians(angle)),py-5*sin(radians(angle))),angle])
+    def renderPlayer(self):
+        self.rect = Game.screen.blit(self.sprites[self.state],self.pos)
 def renderBullets(Game,player,gunType):
     for b in player.bullets:
         noCol = True
@@ -91,7 +95,7 @@ def renderBullets(Game,player,gunType):
                 player.bullets[player.bullets.index(b)] = [(nx,ny),b[1]]
         else:
             del player.bullets[player.bullets.index(b)]
-def renderEnemyBullets(Game,players,gunType):
+def renderEnemyBullets(Game,userplayer,players,gunType):
     for player in players:
         for b in player.bullets:
             noCol = True
@@ -108,6 +112,8 @@ def renderEnemyBullets(Game,players,gunType):
                     lb = transform.rotate(gunType.bulletsprite, b[1])
                     shot = Game.screen.blit(lb, (lx, ly))
                     player.bullets[player.bullets.index(b)] = [(nx, ny), b[1]]
+                    if shot.colliderect(userplayer.rect):
+                        userplayer.takeDamage(gunType.damage)
             else:
                 del player.bullets[player.bullets.index(b)]
 

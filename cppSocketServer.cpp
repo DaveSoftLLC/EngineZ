6bcacc8dfc4144fdc9b912927a74c900cfba5946
JamesXu <xujames007@gmail.com>
2018-05-05 12:37:07 -0400

    stuff

--- cppSocketServer.cpp
+++ cppSocketServer.cpp
@@ -69,11 +69,18 @@ void handle (int sock)
 {
    int n;
    char buffer[256];
-      
    bzero(buffer,256);
-   n = read(sock,buffer,255);
-   if (n < 0) error("ERROR reading from socket");
-   printf("Here is the message: %s\n",buffer);
-   n = write(sock,"I got your message",18);
-   if (n < 0) error("ERROR writing to socket");
+   while(1) {
+    n = read(sock,buffer,255);
+    if (n < 0) {
+      error("ERROR reading from socket");
+      break;
+    }
+    printf("Here is the message: %s\n",buffer);
+    n = write(sock,"I got your message",18);
+    if (n < 0) {
+      error("ERROR writing to socket");
+      break;
+    }
+ }
 }

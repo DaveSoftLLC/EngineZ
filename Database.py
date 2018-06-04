26c236ded3c6e20971d871b0cc91924c241e4304
James Xu <xujames007@gmail.com>
2018-06-03 23:02:43 -0400

    end of day :)

--- Database.py
+++ Database.py
@@ -20,4 +20,11 @@ class Database:
         username, password = pickle.loads(conn.recv(1024))
         obj = Row(username, password, 0)
         conn.close()
-class Server
+class AuthenticateServer:
+    def __init__(self, TCP_IP, TCP_PORT, BUFFER_SIZE):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind((TCP_IP, TCP_PORT))
+        self.s.listen(1)
+        
+    

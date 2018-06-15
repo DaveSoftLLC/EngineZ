1f48460e4224862b132781b0f9a81b68955d6ccd
JamesXu <xujames007@gmail.com>
2018-06-15 09:18:07 -0400

    Improved exit process

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import game
 import menu
+import pygame as pg
+import sys
 running = True
 main = menu.Main()
 mode = 'login'
 username = ''
 while running:
-    if mode == 'login':
+    if mode.lower() == 'login':
         mode, data = main.login_screen()
-    elif mode == 'exit':
+    elif mode.lower() == 'exit':
         running = False
-    elif mode == 'game':
+    elif mode.lower() == 'game':
         mode, data = game.main(data)
-    elif mode == 'menu':
+    elif mode.lower() == 'menu':
         main = menu.Main(data)
         mode, data = main.draw_home()
+pg.quit()
+sys.exit()
 quit()

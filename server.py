c41f5a32e7b35c8cb5460dc86bddd8adc9c80973
fairnightzz <zhehaizhang1@gmail.com>
2018-06-15 11:20:29 -0400

    server comments added

--- server.py
+++ server.py
@@ -23,6 +23,7 @@ class GameInstance:
         self.game = self.instance #alias to be backwards compatible with zhehais code
         self.clients = clients #list of client sockets
         #Gun sprites------------------------------------------------------------------------------------------------
+        #               Gun(Gun name,     gun bullet,              damage,          weapon image,        spread,   fire rate)
         assaultrifle = Gun('AR',image.load('Weapons/lightbullet.png'),5,image.load('Weapons/machinegun.png'),0,0.15)
         shotgun = Gun('Shotgun', image.load('Weapons/shellBullet.png'),5,image.load('Weapons/shotgunb.png'), 6,0)
         sniper = Gun('Sniper',image.load('Weapons/heavyBullet.png'),25,image.load('Weapons/sniper.png'),1,0)
@@ -96,8 +97,6 @@ class GameInstance:
                             for key, value in self.player_health_dict.items():#otherwise replace player health with server side health
                                 self.player_dict[key].health = value
                         #Remove and add weapons
-                        #print(self.player_dict[current_player].weapon_send)
-                        
                         if len(self.player_dict[current_player].weapon_send) > 0:
                             if self.player_dict[current_player].weapon_send[1] == 0:#Picking up weapon
                                 del self.weapon_map[self.weapon_map.index(self.player_dict[current_player].weapon_send[0])]
@@ -154,20 +153,21 @@ class GameInstance:
             g = self.game
             name = list(self.player_dict.keys())
             obj = list(self.player_dict.values())
-            players = dict(zip(name,obj))
+            players = dict(zip(name,obj))#To prevent dictionary error
             for name, obj in players.items(): #Loop through each player
                 try:
-                    x = self.storm_pos[self.storm_state][0]-obj.pos[0]
-                    y = self.storm_pos[self.storm_state][1]-obj.pos[1] 
+                    x = self.storm_pos[self.storm_state][0]-obj.pos[0] 
+                    y = self.storm_pos[self.storm_state][1]-obj.pos[1]
+                    #If the player is outside of the storm and the time for taking damage is more than 1 second
                     if hypot(x,y)>self.storm_rad[self.storm_state] and t.time()-self.dam>1:
                         self.dam = t.time()
                         if self.player_health_dict[name] - 1 >= 0:
                             self.player_health_dict[name] -= 1
                 except:
                     pass
-                        #print("OUTSIDE STORM")
+
                 for b in obj.bullets: #Loop through each of their bullets
-                    for p in [i for i in self.player_dict.values()]: #Loop through the other players
+                    for p in [i for i in self.player_dict.values()]: #Loop through the players
                         if name == p.name: #Skip if its the same player
                             continue
                         if name in del_bullets.keys(): #Make sure it wasnt marked for deletion
@@ -181,8 +181,9 @@ class GameInstance:
                         angle = b[1]
                         interpolate = [(nx - i * cos(radians(angle)),
                                         ny + i * sin(radians(angle))) for i in range(b[3])] #Interpolate gaps between each +20ish on pos
-##                        interpolate = gameMath.interpolate(int(nx),int(ny),int(angle),int(b[3]))
-                        for ix, iy in interpolate:
+
+                        
+                        for ix, iy in interpolate:#Checks everything in between +20ish pixels (to prevent bullet from skipping over due to lag)
                             if hypot(px - nx, py - ny) < 30:
                                 threading.Thread(target=serverRequest.modify, args=(name, 1)).start()
                                 obj.bullets.remove(b)
@@ -192,8 +193,6 @@ class GameInstance:
                                     del_bullets[name].append(b)
                                 if self.player_health_dict[p.name] - self.weapon_dict[b[2]].damage > 0: #Take away health
                                     self.player_health_dict[p.name] -= self.weapon_dict[b[2]].damage
-##                                elif self.player_health_dict[p.name]<10:
-##                                    self.player_health_dict[p.name] = 0
                                 else:
                                     threading.Thread(target=serverRequest.modify, args=(name, 5)).start()#If kill, add more points
                                     self.player_health_dict[p.name] = 0
@@ -218,41 +217,39 @@ class GameInstance:
         except ValueError:
             pass
     def storm(self,start = False):
-        if start:
+        if start:#Starting storm for the first time
             self.storm_time = t.time()
         else:
             while self.running:
+                #Every 60 seconds, switch storm to idle and then to moving
                 if t.time()-self.storm_time>60:
                     self.storm_time = t.time()
                     if self.storm_next == "idle":
                         self.storm_next = "moving"
                         self.storm_rad[self.storm_state]
+                        #The continuous change in which the storm will be moving
                         self.x = ((self.storm_pos[self.storm_state+1][0]-self.storm_pos[self.storm_state][0])/(600))
                         self.y = ((self.storm_pos[self.storm_state+1][1]-self.storm_pos[self.storm_state][1])/(600))
-                        print(self.x, self.y)
-                        print(self.storm_pos)
                         self.r = ((self.storm_rad[self.storm_state]-self.storm_rad[self.storm_state+1])/600)
-                        print(self.r)
                         self.storm_moving = 0
                     
                     else:
+                        #Sets storm back to idle
                         self.storm_next = "idle"
+                        #Prevent storm state from going over the list
                         if self.storm_state+2 != len(self.storm_rad):
                             self.storm_state += 1
                         else:
-                            print("storm is done")
+                            #End storm
                             break
                     
-                    print("THE STORM")
-                if t.time()-self.storm_moving>.1 and self.storm_next == "moving":
+                if t.time()-self.storm_moving>.1 and self.storm_next == "moving":#Storm updates every .1 seconds
                     self.storm_moving = t.time()
+                    #Moves the storm accordingly
                     if (self.storm_state+1) != len(self.storm_rad) and self.storm_rad[self.storm_state] >= self.storm_rad[self.storm_state+1]:
                         self.storm_pos[self.storm_state][0]+=self.x
                         self.storm_pos[self.storm_state][1]+=self.y
                         self.storm_rad[self.storm_state]-= self.r
-                        #print(self.storm_rad,self.storm_pos)
-                        #print("moving")
-
 
 class Server:
     def __init__(self, BUFFER_SIZE):

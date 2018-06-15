b3991eb4ee5c1c5b96fcc35c291a7b17fe64b814
JamesXu <xujames007@gmail.com>
2018-06-15 10:22:39 -0400

    Enhanced comments

--- authenticate.py
+++ authenticate.py
@@ -14,47 +14,44 @@ class MySQLRequest:
         self.database = database
 
     def select(self, table, username):
+        'Selects password hash from table'
         db = sql.connect(host=self.host,
                          user=self.username,
                          password=self.password,
                          db=self.database)
-
+        #^^^ Connect to database
         cur = db.cursor()
-        cur.execute("SELECT password_hash FROM " + table + " WHERE username='" + username + "'")
-        if cur.rowcount < 1:
+        cur.execute("SELECT password_hash FROM " + table + " WHERE username='" + username + "'") #Execute query
+        if cur.rowcount < 1: #Make sure there's a result
             return False
         else:
-            for row in cur.fetchall():
-                return row[0]
+            for row in cur.fetchall():#Should only have one row as usernames are unique
+                return row[0] #Only returns password_hash, so only one result
         db.close()
     def insert(self,table, username, password):
+        'Inserts a new user into database'
         ph = PasswordHasher()
         db = sql.connect(host=self.host,
                          user=self.username,
                          password=self.password,
                          db=self.database)
         cur = db.cursor()
-        hashed_password = ph.hash(password)
-        cur.execute("INSERT INTO %s (username,password_hash,highscore) VALUES ('%s','%s',0)"
+        hashed_password = ph.hash(password) #Hashed password
+        cur.execute("INSERT INTO %s (username,password_hash,highscore) VALUES ('%s','%s',0)" #Execute insertion query
                     %(table,username,hashed_password))
-        db.commit()
+        db.commit()#Commit changes
         db.close()
     def modify(self,username,score):
+        'Adds new score to current score'
         db = sql.connect(host=self.host,
                          user=self.username,
                          password=self.password,
                          db=self.database)
         cur = db.cursor()
-        cur.execute("SELECT highscore FROM users WHERE username='%s';" %username)
+        cur.execute("SELECT highscore FROM users WHERE username='%s';" %username) #Make sure user exists
         if cur.rowcount != 1:
-            raise ValueError('Username not Found/Unique')
+            raise ValueError('Username not Found/Unique') #Raise an error if more or less than one user exists
         old_score = cur.fetchall()[0][0]
-        cur.execute("UPDATE users SET highscore = %d WHERE username = '%s';" %(old_score+score,username))
-        db.commit()
+        cur.execute("UPDATE users SET highscore = %d WHERE username = '%s';" %(old_score+score,username))#Update 
+        db.commit()#Commit changes
         db.close()
-        
-        
-if __name__ == '__main__':
-    d = MySQLRequest('159.203.101.135', 'jamesxu', 'enginez123', 'enginez')
-##    print(d.select('users','pay2lose'))
-    d.modify('pay2lose',100)
